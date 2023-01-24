867547382f65281e3215a649c2141b78836d2d1a
Rahangdale <yogesh.rahangdale@capgemini.com>
2023-01-24 16:04:46 +0530

    first commit

---
+++ src/main/java/com/bootapp/rest/restapp/OnlineBankingSprint1Application.java
@@ -0,0 +1,13 @@
+package com.bootapp.rest.restapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingSprint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingSprint1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootapp/rest/restapp/OnlineBankingSprint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootapp.rest.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSprint1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
